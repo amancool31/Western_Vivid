07b32379974c54965adb05d60648989536a3eb75
Anish110910 <anish.mendiratta@gmail.com>
2018-10-16 22:16:25 +0530

    Minor changes

--- src/components/itemDisplay.js
+++ src/components/itemDisplay.js
@@ -1,6 +1,8 @@
 import React, { Component} from 'react';
-import {Card,Grid,Button,Input,Select } from 'semantic-ui-react';
+import {Card,Grid,Button,Input,Select,Image } from 'semantic-ui-react';
 import firebase from 'firebase';
+
+  
   
 class ItemDisplay extends Component {
     constructor(props)
@@ -10,10 +12,7 @@ class ItemDisplay extends Component {
             items:this.props.content,
             cart:[],
             sortedItems:[],
-            // dummyPriceMin:0,
-            // dummyPriceMax:1000,
-            // priceMax:10000,
-            // priceMin:0,
+            
             searchResult:'',
             loggedIn:false
         }
@@ -44,8 +43,8 @@ class ItemDisplay extends Component {
         cartitems.push(item)
         itemsList[index].cart=true;
         this.setState({items:itemsList,cart:cartitems});
-        console.log(this.state.cart)
-         
+          
+
          
     }
     removeFromCart(item,index)
@@ -64,12 +63,10 @@ class ItemDisplay extends Component {
     render() {
         return (
             <div>
-                 
-                    
-                
-            <Grid>
-            
-            <Grid.Row centered>
+                <center>
+                <Grid celled='internally'>
+             
+            <Grid.Row  centered style={{marginBottom: 10}}>
             {
                     this.state.cart.length!=0 && this.state.loggedIn==true?
                      <Button color='green'>PROCEED TO CHECKOUT</Button>  
@@ -77,88 +74,85 @@ class ItemDisplay extends Component {
                     null
                 } 
              
-            </Grid.Row>
-            {/* <Grid.Row columns={4}>
-                <Grid.Column>
-                     
-                        <Input placeholder='Minimum' value={this.state.dummyPriceMin} onChange={(text)=>
-                            {this.setState({dummyPriceMin:text.target.value})}}/>
-                        <Input placeholder='Maximum' value={this.state.dummyPriceMax} onChange={(text)=>{
-                            this.setState({dummyPriceMax:text.target.value})
-                            console.log(this.state.dummyPriceMax)
-                            }}/>
-                    
-                </Grid.Column>
-                <Grid.Column>
-                        
-                </Grid.Column>
-                <Grid.Column>
-                        
-                </Grid.Column>
-                <Grid.Column>
-                       <Button color='red' onClick={()=>{this.onModify()}}>MODIFY</Button> 
-                </Grid.Column>
-                
-            </Grid.Row> */}
-                {/* Unsorted */}
-                 <Grid.Row  columns={4}>
-                 {this.state.items.map((i,j)=>{
-                return(
-                    
-                        
-                         
-                           <Grid.Column key={j}> <Card  fluid style={{margin: 5}}>
-                             
-                            <Card.Content>
-                                <p>{i.name}</p>
-                                <img  width='100%' src='https://picsum.photos/200' />
-                                <h2>₹ {i.price}</h2>
-                                <h3>{i.size}</h3>
-                                {
-                                    this.state.loggedIn==true && firebase.auth().currentUser.emailVerified==true?
-                                    <div>
-                                {
-                                    i.cart==false?
-                                    <Button fluid onClick={()=>{this.pushToCart(i,j)}} inverted color='red'>ADD TO CART</Button>
-                                    :
-                                     
-                                    <Button fluid onClick={()=>{this.removeFromCart(i,j)}}   color='red'>REMOVE FROM CART</Button>
-                                     
-                                }
-                                </div>
-                                :
-                                <Button fluid onClick={()=>{
-                                if(this.state.loggedIn==false)
-                                {
-                                    alert('Please Log In')
-                                }
-                                else if(this.state.loggedIn==true && firebase.auth().currentUser.emailVerified==false)
-                                {
-                                    alert('Your account is not verified . A verification link has been sent to your inbox at '+firebase.auth().currentUser.email)
-                                }    
-                                }} inverted color='red'>ADD TO CART</Button>
-                                }
-                            </Card.Content>
-                       </Card>  </Grid.Column>
-                        
-                         
-                         
-                        
-                         
-                       
-                )
-            })} 
-          
-            
-                 </Grid.Row>
+            </Grid.Row> 
+        
+       
+                 
+
+                <Grid.Row  columns={3}>
+                <br/><br/>
                   
-            </Grid><br/><br/>
+                  {this.state.items.map((i,j)=>{
+                 return(
+                     
+                       
+                              
+                          
+                          <Grid.Column key={j}> <Card>
+                            
+                             <Card.Content>
+                               <p>{i.name}</p>
+                               <Image src='https://picsum.photos/200' />
+                               <h3>₹ {i.price}</h3>
+                               <h4>{i.size}</h4>
+                               {
+                                   this.state.loggedIn==true && firebase.auth().currentUser.emailVerified==true?
+                                   <div>
+                               {
+                                   i.cart==false?
+                                   <Button   onClick={()=>{this.pushToCart(i,j)}} inverted color='red'>ADD TO CART</Button>
+                                   :
+                                    
+                                   <Button  onClick={()=>{this.removeFromCart(i,j)}}   color='red'>REMOVE FROM CART</Button>
+                                    
+                               }
+                               </div>
+                               :
+                               <Button  onClick={()=>{
+                               if(this.state.loggedIn==false)
+                               {
+                                   alert('Please Log In')
+                               }
+                               else if(this.state.loggedIn==true && firebase.auth().currentUser.emailVerified==false)
+                               {
+                                   alert('Your account is not verified . A verification link has been sent to your inbox at '+firebase.auth().currentUser.email)
+                               }    
+                               }} inverted color='red'>ADD TO CART</Button>
+                               }
+                           </Card.Content>
+                      </Card>  </Grid.Column>
+                        
+                         
+                          
+                          
+                         
+                          
+                        
+                 )
+             })} 
+                   
+           
+             
+                  </Grid.Row>
+                
+                 
+                {/* <Grid.Column width={1} floated='right'><Card  >sfdfdsfd</Card></Grid.Column> */}
+     
+                 
+                   
+                   
+            </Grid>
+                    </center> 
+                    
+                
+           <br/><br/>
             {
                     this.state.cart.length!=0?
                     <Button color='green'>PROCEED TO CHECKOUT</Button>
                     :
                     null
                 }
+                
             </div>
         );
     }
