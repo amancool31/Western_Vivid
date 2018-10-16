b90dd68d51f76a11b7cab8a8a0a85b1653e958f6
Anish110910 <anish.mendiratta@gmail.com>
2018-10-16 20:46:58 +0530

    Added Loading Screen

--- src/App.js
+++ src/App.js
@@ -2,12 +2,46 @@ import React, { Component } from 'react';
 import './App.css';
 import {Segment,Grid,Button} from 'semantic-ui-react';
 import Main from './components/mainHeader';
+import {Loader,Dimmer} from 'semantic-ui-react';
+import auth from "./Firebase_config";
+import firebase from 'firebase';
 
 class App extends Component {
+  constructor()
+  {
+    super();
+    this.state={
+      rendr:false
+    }
+  }
+  componentDidMount()
+  {
+    firebase.auth().onAuthStateChanged((user)=>{
+      if(user)
+      {
+        this.setState({rendr:true})
+      }
+      else
+      {
+        setTimeout(() => {
+          this.setState({rendr:true})
+        }, 1000);
+      }
+    })
+  }
   render() {
     return (
       <div className="App">
-         <Main />
+          
+           {
+             this.state.rendr==false?
+             <Dimmer active inverted >
+               <Loader>Loading..</Loader>
+             </Dimmer>
+             :
+             <Main />
+           }
+        
       </div>
     );
   }
