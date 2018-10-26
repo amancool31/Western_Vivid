e0a0361a62633d524067ff4056ef22d764f33148
Anish110910 <anish.mendiratta@gmail.com>
2018-10-27 01:49:06 +0530

    Created product page UI

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {  Grid,Image ,Loader ,Dimmer} from 'semantic-ui-react'
+import {  Grid,Image ,Loader ,Dimmer ,Card ,Label, Input , Table,Button ,Icon ,Popup} from 'semantic-ui-react'
 // import styled from 'styled-components';
 
 // const Div = styled.div`
@@ -33,31 +33,106 @@ import {  Grid,Image ,Loader ,Dimmer} from 'semantic-ui-react'
     render() {
          
       return (
-        <Grid >
-      <Grid.Row columns={2}>
-        <Grid.Column width={5}>
+        
+       <div>
+         <br/> 
+        <Grid centered>
+      <Grid.Column width={6} >
          
          
-          <Image src={this.props.data.img} size='large'  rounded />
-       
+         
+         
+         <Image  fluid src={this.props.data.img} size='large'   rounded 
+          
+         />
+          
+          
         </Grid.Column>
-        <Grid.Column width={11}>
-          {/* <Basictext /> */}
-        </Grid.Column>
-      </Grid.Row>
+        <Grid.Column width={7}>
+        
+        <h1>{this.props.data.title} 
+          <Popup 
+          trigger={<Button icon='heart outline' inverted floated='right'   color='red' />}
+          content='Add to your wishlist'
+          />
+        </h1>
+        <Label size='tiny' color='red'>25% OFF</Label><br/> 
+        <h2>₹ {this.props.data.price}</h2>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
+        a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+         remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+         Lorem Ipsum passages, and more 
+        recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
+        a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+         remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+         Lorem Ipsum passages, and more 
+        recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        
+        {/* <Label >Choose Quantity <Input inverted type='number'  /></Label> */}
+         <Button fluid color='blue'>
+         <Icon name='shopping cart'/>
+           ADD TO CART
+         </Button><br/>
+         <Button fluid color='red'>
+           BUY NOW
+         </Button>
+        
+                
+             </Grid.Column>  
+             </Grid>
+             <br/><br/><br/><br/>
+         <Grid>
+           <h2>
+             ITEM DETAILS
+           </h2><br/><br/>
+          <Table definition>
+           
+
+          <Table.Body>
+      <Table.Row>
+        <Table.Cell>Product Code</Table.Cell>
+        <Table.Cell>Price</Table.Cell>
+        <Table.Cell>Size</Table.Cell>
+        <Table.Cell>Fit</Table.Cell>
+        <Table.Cell>Colors</Table.Cell>
+        
+
+
+
+      </Table.Row>
+      <Table.Row>
+      <Table.Cell>{this.props.data.code}</Table.Cell>
+        <Table.Cell>₹ {this.props.data.price}</Table.Cell>
+        <Table.Cell>L</Table.Cell>
+        <Table.Cell>{this.props.data.fit}</Table.Cell>
+        <Table.Cell>Blue,Red,Black</Table.Cell>
+         
+      </Table.Row>
+    </Table.Body>
+          </Table>
+         </Grid><br/>
+         <br/>
+       </div>
+             
+        
   
-      <Grid.Row columns={6}>
-        <Grid.Column width={3}>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-        </Grid.Column>
-        <Grid.Column width={10}>
-          <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-        </Grid.Column>
-        <Grid.Column width={3}>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+    //   /*{ <Grid.Row columns={6}>
+    //     <Grid.Column width={3}>
+    //       <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+    //     </Grid.Column>
+    //     <Grid.Column width={10}>
+    //       <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+    //     </Grid.Column>
+    //     <Grid.Column width={3}>
+    //       <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+    //     </Grid.Column>
+    //   </Grid.Row> */}
+    // </Grid>
       );
     }
   }
